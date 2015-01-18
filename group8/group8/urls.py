907f1b4a5812a3ef76008b97da1a9d88a5545df9
Amm752 <Amm752@nau.edu>
2015-01-18 10:41:28 -0700

    Update urls.py

--- group8/group8/urls.py
+++ group8/group8/urls.py
@@ -1,20 +1,14 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
 
-def home(request):
-    return render(request, 'homePage.html', {})
+urlpatterns = patterns('',
+    url(r'^$', 'group8.views.home', name='home'),
+    url(r'^homePage', 'group8.views.home', name='home'),
+    url(r'^biography', 'group8.views.biography', name='biography'),
+    url(r'^Discography', 'group8.views.Discography', name='Discography'),
+    url(r'^Tour', 'group8.views.Tour', name='Tour'),
+    url(r'^SocialMedia', 'group8.views.SocialMedia', name='SocialMedia'),
+    url(r'^upcoming', 'group8.views.upcoming', name='upcoming'),
 
-def biography(request):
-    return render(request, 'biography.html', {})
-
-def Discography(request):
-    return render(request, 'discography.html', {})
-
-def Tour(request):
-    return render(request, 'Tour.html', {})
-
-def SocialMedia(request):
-    return render(request, 'SocialMedia.html', {})
-
-def upcoming(request):
-    return render(request, 'SocialMedia.html', {})
+    url(r'^admin/', include(admin.site.urls)),
+)

03155e2482be23c2cfe93b499269b7beb5f00418
Amm752 <Amm752@nau.edu>
2015-01-18 10:41:47 -0700

    Update views.py

--- group8/group8/views.py
+++ group8/group8/views.py
@@ -3,3 +3,18 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'homePage.html', {})
+
+def biography(request):
+    return render(request, 'biography.html', {})
+
+def Discography(request):
+    return render(request, 'discography.html', {})
+
+def Tour(request):
+    return render(request, 'Tour.html', {})
+
+def SocialMedia(request):
+    return render(request, 'SocialMedia.html', {})
+
+def upcoming(request):
+    return render(request, 'SocialMedia.html', {})

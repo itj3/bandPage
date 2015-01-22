0c17bc9d2e960d78c93e70880aa8cb962b63dd6d
Amm752 <am752@nau.edu>
2015-01-22 15:54:19 -0700

    My final Project

--- group8/group8/urls.py
+++ group8/group8/urls.py
@@ -3,6 +3,12 @@ from django.contrib import admin
 
 urlpatterns = patterns('',
     url(r'^$', 'group8.views.home', name='home'),
+    url(r'^homePage', 'group8.views.home', name='home'),
+    url(r'^biography', 'group8.views.biography', name='biography'),
+    url(r'^Discography', 'group8.views.Discography', name='Discography'),
+    url(r'^Tour', 'group8.views.Tour', name='Tour'),
+    url(r'^SocialMedia', 'group8.views.SocialMedia', name='SocialMedia'),
+    url(r'^upcoming', 'group8.views.upcoming', name='upcoming'),
 
     url(r'^admin/', include(admin.site.urls)),
 )
---
+++ group8/static/js/Popup.js
@@ -0,0 +1,3 @@
+function alertUser(msg) {
+alert(msg);
+}
